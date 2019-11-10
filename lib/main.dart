ea3d9d456b4562a511e24f8fd10973df933d5c51
Emrullah Ayın <ayinemrullah@gmail.com>
2019-11-10 15:11:31 +0300

    add font

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: "My First Title",
-      home: Material(
-          child: Center(
-        child: Text(
-          "Hello World!",
-          textDirection: TextDirection.ltr,
-        ),
+  runApp(HelloWorldApp());
+}
+
+class HelloWorldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        title: "My First Flutter App",
+        theme: ThemeData(fontFamily: "McLaren"),
+        home: Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.teal, title: Text("AppBar Title")),
+            body: MainPage()));
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+      color: Colors.tealAccent,
+      child: Center(
+          child: Text(
+        "Hello World!",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.black, fontSize: 50.0),
       )),
-    ),
-  );
+    );
+  }
 }
